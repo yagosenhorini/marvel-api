7be43f409bd21064984591a52d9bf35deb26e70a
Yago  Senhorini <yagosenhorini@MacBook-Pro-de-Yago.local>
2020-07-18 16:24:06 -0300

    feat: finish modal

---
+++ src/Components/Modal/index.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect, useRef} from 'react';
+import * as S from './style';
+
+export default function Modal({props}, isActive){
+ 
+const [useHero, setHero] = useState(null);
+const [useActive, setActive] = useState(null);
+
+
+const modalRef = useRef();
+
+  useEffect(() => {
+    setHero(props);
+    setActive(isActive); 
+  }, [isActive, props]);
+
+  function activeModal(){
+      setActive(false);
+  }
+  
+  return(
+    <>
+      {useHero &&  useActive &&(
+        <S.Modal ref={modalRef} isActive={useActive}>
+          <S.CloseBtn onClick={() => activeModal()}>X</S.CloseBtn>
+          <S.Name>{useHero.name}</S.Name>
+          <S.ImageWrapper>
+            <S.Image src={`${useHero.thumbnail.path}.${useHero.thumbnail.extension}`}></S.Image>
+          </S.ImageWrapper>
+          <S.Description>{useHero.description ? useHero.description : 'N/A' }</S.Description>
+        </S.Modal>
+      )}
+    </>
+  );
+};
---
+++ src/Components/Modal/style.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const Modal = styled.div`
+  top: 25%;
+  left: 10%;
+  width: 80%;
+  right: 10%;
+  bottom: 25%;
+  height: auto;
+  position: fixed;
+  border-radius: 5px;
+  border: 1px solid black;
+  background-color: lightgray;
+  @media screen and (min-width: 1024px){
+    left: 25%;
+    right: 25%;
+    width: 50%;
+  }
+`;
+
+export const Name = styled.h3`
+  width: 50%;
+  font-size: 14px;
+  text-align: center;
+  margin: 10px auto 0;
+  font-family: sans-serif;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 40%;
+  margin: 20px auto;
+  @media screen and (min-width: 1024px){
+    width: 15%;
+  }
+`
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+export const Description = styled.p`
+  width: 90%;
+  height: auto;
+  font-size: 16px;
+  margin: 25px auto;
+  text-align: center;
+  @media screen and (min-width: 1024px){
+    width: 75%;
+    font-size: 18px;
+  }
+`;
+
+export const CloseBtn = styled.button`
+  top: 0;
+  right: 0;
+  width: 40px;
+  color: #000;
+  height: 40px;
+  border: none;
+  font-size: 18px;
+  position: absolute;
+  background-color: transparent
+` 
